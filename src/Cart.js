f6f6f99e9707ec9431cde9f5a6d2f6eeaedca701
manivasagam <s.kishore123.64@gmail.com>
2022-10-06 08:00:55 +0530

    quantity added

--- src/Cart.js
+++ src/Cart.js
@@ -1,23 +1,31 @@
 import React from 'react'
 
-function Cart({ CartItems, removeCart }) {
+function Cart({ CartItems, removeCart,NumOfItems,addItems,removeItems }) {
     return (
        <>
        {
         CartItems.length === 0 ? "No Items in Cart" :
        <>
 
-            <ol class="list-group list-group-numbered">
+            <ol className="list-group list-group-numbered">
                 {
                     CartItems.map((item) => {
-                        return <li class="list-group-item d-flex justify-content-between align-items-start">
-                            <div class="ms-2 me-auto">
-                                <div class="fw-bold">{item.name}</div>
+                        return <li className="list-group-item d-flex justify-content-between align-items-start">
+                            <div className="ms-2 me-auto">
+                                <div className="fw-bold">{item.name}</div>
                                 Rs:{item.price}
                             </div>
+                            <span>
+                                <button onClick={()=>{
+                                
+                                    addItems(item)}}>+</button>
+                                <span>{item.quantity}</span>
+                                <button onClick={()=>{
+                                    removeItems(item)}}>-</button>
+                                </span>
                             <span onClick={() => {
                                 removeCart(item);
-                            }} class="badge bg-primary rounded-pill">X</span>
+                            }} className="badge bg-primary rounded-pill">X</span>
                         </li>
                     })
                 }
@@ -25,7 +33,7 @@ function Cart({ CartItems, removeCart }) {
             </ol>
             <h3>Total Rs= {
                 CartItems.reduce((acc, cur) => {
-                    return acc = acc + cur.price
+                    return acc = acc + (cur.price * cur.quantity)
                 }, 0)
 
             }</h3>
